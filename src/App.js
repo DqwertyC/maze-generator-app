95e1c12851a6599b3f030a42cb4046fda63c4ff8
drewdcamp <dqwertyc.dev@gmail.com>
2024-07-02 14:15:18 -0700

    feat: style changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import useWindowDimensions from "./hooks/useWindowDimensions";
 
 function App() {
+  const [className, setClassName] = React.useState("game-narrow");
+
+  const { height, width } = useWindowDimensions();
+
   const { unityProvider } = useUnityContext({
     loaderUrl: "unity/PlayerBuild.loader.js",
     dataUrl: "unity/PlayerBuild.data",
@@ -10,11 +15,15 @@ function App() {
     codeUrl: "unity/PlayerBuild.wasm",
   });
 
+  React.useEffect(() => {
+    const aspect = height / width;
+    if (aspect > 1.2) setClassName("game-narrow");
+    else setClassName("game-wide");
+  }, [width, height]);
+
   return (
     <div className="App">
-      <div className="game-container">
-        <Unity className="game" unityProvider={unityProvider} />
-      </div>
+      <Unity className={className} unityProvider={unityProvider} />
     </div>
   );
 }
